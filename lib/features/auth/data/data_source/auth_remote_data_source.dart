aade0b6a4d5a2c141e8df7001efcd1640853f715
malak1221 <loka.ashraf2244@gmail.com>
2026-09-20 01:54:56 +0300

    mmm

--- lib/features/auth/data/data_source/auth_remote_data_source.dart
+++ lib/features/auth/data/data_source/auth_remote_data_source.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-import '/core/errors/failure.dart';
+import '../core/errors/failure.dart';
 import '../models/login_req.dart';
 import '../models/signup_req.dart';
 import '../models/verify_req.dart';
--- lib/features/auth/data/data_source/auth_remote_data_source_imp.dart
+++ lib/features/auth/data/data_source/auth_remote_data_source_imp.dart
@@ -1,10 +1,10 @@
 import 'package:dartz/dartz.dart';
-import 'package:mm_2/core/errors/failure.dart';
+import 'package:mm_2/core/errors/faliure.dart';
 import 'package:mm_2/core/network/api_constants.dart';
+import 'package:mm_2/core/network/api_consumer.dart';
 import 'package:mm_2/features/auth/data/data_source/auth_remote_data_source.dart';
 
 class AuthDataSourceImpl implements AuthDataSource {
-
   final ApiConsumer apiConsumer;
 
   AuthDataSourceImpl(this.apiConsumer);
@@ -14,18 +14,62 @@ class AuthDataSourceImpl implements AuthDataSource {
     required String email,
     required String password,
   }) async {
-    final response = await  apiConsumer.post(
-      path: '/login',
-      body: {
+    final response = await apiConsumer.post(
+      path: ApiConstants.login,
+      data: {
         'email': email,
         'password': password,
       },
     );
+
     return response.fold(
           (failure) => Left(failure),
           (data) => Right(data),
     );
   }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> signUp({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+  }) async {
+    final response = await apiConsumer.post(
+      path: ApiConstants.register,
+      data: {
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+        'password': password,
+      },
+    );
+
+    return response.fold(
+          (failure) => Left(failure),
+          (data) => Right(data),
+    );
+  }
+
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> verify({
+    required String email,
+    required String code,
+  }) async {
+    final response = await apiConsumer.post(
+      path: ApiConstants.verifyEmail,
+      data: {
+        'email': email,
+        'otp': code,
+      },
+    );
+
+    return response.fold(
+          (failure) => Left(failure),
+          (data) => Right(data),
+    );
+  }
+}
 
   @override
   Future<Either<Failure, Map<String, dynamic>>> signUp({
